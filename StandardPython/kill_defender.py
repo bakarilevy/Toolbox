47c6e7bec3d4af41d69a80c406fed01397da8c52
exec life <exec_life@protonmail.com>
2022-03-06 13:26:59 -0600

    Adding scaffold for Python implementation of Pwn1sher's KillDefender

---
+++ StandardPython/kill_defender.py
@@ -0,0 +1,17 @@
+import os
+
+
+def enable_debug_privilege():
+    pass
+
+def get_pid():
+    pass
+
+def set_privilege():
+    pass
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
